e3abab22e41b92517d1c0fdf5ead20016303d9a6
esther <jiafq@hsmdata.com>
2016-12-20 15:20:20 +0800

    提交

---
+++ src/main/java/com/hsmdata/springTest/common/framework/context/CacheContext.java
@@ -0,0 +1,5 @@
+package com.hsmdata.springTest.common.framework.context;
+
+public class CacheContext {
+
+}
---
+++ src/main/java/com/hsmdata/springTest/common/utils/PropertiesUtil.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.hsmdata.springTest.common.utils;
+
+/**
+ * @author E2
+ *
+ */
+public class PropertiesUtil {
+
+}
