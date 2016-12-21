a9d112e330b220aaf99c19fbc78dfa4b415ae051
esther <jiafq@hsmdata.com>
2016-12-21 16:41:56 +0800

    user

---
+++ src/main/java/com/hsmdata/springTest/modules/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.hsmdata.springTest.modules.mapper;
+
+import com.hsmdata.springTest.modules.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
