29ee0542c59c53adfdb03c732720de0305ae00bc
esther <jiafq@hsmdata.com>
2017-02-22 11:12:26 +0800

    修改

---
+++ src/main/java/com/hsmdata/springTest/modules/test/DictTest.java
@@ -0,0 +1,24 @@
+package com.hsmdata.springTest.modules.test;
+
+import com.hsmdata.springTest.common.utils.HttpClientUtil;
+
+/**
+ * 字典测试
+ */
+public class DictTest {
+    public static void main(String[] args) {
+       // testAllDictList();
+        testDictListByDictType();
+    }
+
+    @SuppressWarnings("unused")
+    private static void testAllDictList() {
+        String url = "http://localhost:8080/springTest/dict/list/all";       
+        HttpClientUtil.sendPost(url, "");
+    }
+    
+    private static void testDictListByDictType() {
+        String url = "http://localhost:8080/springTest/dict/list/some?dictType=COMPANY";
+        HttpClientUtil.sendHttpsGet(url);
+    }
+}
