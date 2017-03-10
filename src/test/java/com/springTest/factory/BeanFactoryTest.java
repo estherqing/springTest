df78b3ae7d1dbb6e399aa0b5c6f8149eedc32db7
esther <jiafq@hsmdata.com>
2017-03-10 17:36:34 +0800

    BeanFactory

---
+++ src/test/java/com/springTest/factory/BeanFactoryTest.java
@@ -0,0 +1,36 @@
+package com.springTest.factory;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+import com.hsmdata.springTest.modules.aop.HelloWorld;
+
+import junit.framework.Assert;
+
+public class BeanFactoryTest {
+
+	public static void main(String[] args) {
+		ClassPathResource resource = new ClassPathResource("/config/spring-servlet.xml");
+		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+		reader.loadBeanDefinitions(resource);
+		HelloWorld hw3 = (HelloWorld) factory.getBean("helloWorldImp1");
+		hw3.printHelloWorld();
+		/**
+		ApplicationContext ctx = new FileSystemXmlApplicationContext("");
+		HelloWorld hw1 = (HelloWorld) ctx.getBean("helloWorldImp1"); 
+		hw1.printHelloWorld();
+		System.out.println();
+		hw1.doPrint();
+		**/
+		 
+		//Assert.assertEquals(2, 3);
+		String str = null;
+		Assert.assertNotNull("str is not null",str);
+	}
+
+}
