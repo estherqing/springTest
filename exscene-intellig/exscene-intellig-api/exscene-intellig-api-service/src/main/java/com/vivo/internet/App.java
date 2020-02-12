d928165c4d8ad1b2266a8acf3b5066641b648906
jiafuqing <fuqing.jia@vivo.com>
2020-02-12 14:57:07 +0800

    init

---
+++ exscene-intellig/exscene-intellig-api/exscene-intellig-api-service/src/main/java/com/vivo/internet/App.java
@@ -0,0 +1,13 @@
+package com.vivo.internet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
